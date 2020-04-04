f8c4a8af1c95ea52c4478394bdaaa1466a66c735
rsepic <rmsepic@ucdavis.edu>
2020-04-04 16:39:04 -0700

    This is the beginning of the design of the website, with styling and a designed layout.
    The frontend fo the website has been combined with the Django application.
    
    Home page, about page.

--- PP_APP/server/frontend/urls.py
+++ PP_APP/server/frontend/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 from .views import (
     home_view,
+    about_view,
+    upload_view
+
 )
 
-app_name = 'frontend'
+#app_name = 'frontend'
 
 urlpatterns = [
-    path('', home_view, name='home'),
+    path('home/', home_view, name='home'),
+    path('about/', about_view, name='about'),
+    path('upload/', upload_view, name='upload')
 ]
