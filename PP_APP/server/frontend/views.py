7020fc5cfb7dbca0b0174ad6182663f19fea06f1
rsepic <rmsepic@ucdavis.edu>
2020-04-28 21:24:55 -0700

    The frontend is integrated with the Django application fully, including urls. Success page is added and the about page includes the names, linkedins, and githubs or all the members, as well as the abstract that was submitted for the design showcase.
    Removed the website directory, which held the original design, containing only html, css, and scss documents.
    
    Updated the about page with our names, linkedins and githubs, as well as the abstract that will be shown at the design showcase.  Added the success page.

--- PP_APP/server/frontend/views.py
+++ PP_APP/server/frontend/views.py
@@ -9,3 +9,6 @@ def home_view(request):
 
 def upload_view(request):
     return render(request, 'frontend/upload.html', {'title' : 'Upload'})
+
+def success_view(request):
+    return render(request, 'frontend/success.html', {'title' : 'Success!'})
