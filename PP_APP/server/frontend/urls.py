54869367e782f3836f203f6af3fc8a355c1a698e
ShafiTek <yoosuf290@yahoo.com>
2020-05-03 20:58:43 -0700

    Slight redesign of Ryland's frontend.

--- PP_APP/server/frontend/urls.py
+++ PP_APP/server/frontend/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views import (
     home_view,
+    rate_view,
+    feed_view,
+    ranking_view,
     about_view,
     upload_view,
     success_view,
@@ -12,7 +15,9 @@ from .views import (
 
 urlpatterns = [
     path('', home_view, name='home'),
-    path('home/', home_view, name='home'),
+    path('rate/', rate_view, name='rate'),
+    path('feed/', feed_view, name='feed'),
+    path('ranking/', ranking_view, name='ranking'),
     path('about/', about_view, name='about'),
     path('upload/', upload_view, name='upload'),
     path('success/', success_view, name='success'),
